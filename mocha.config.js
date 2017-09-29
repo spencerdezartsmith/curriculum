9836973d1875f995faf7b395abe9889587dab62f
spencerdezartsmith <spencer.dezart.smith@gmail.com>
2017-09-29 11:37:26 -0700

    refactored authentication to support login backdoor in test moved to cookies from cookie-parser

--- mocha.config.js
+++ mocha.config.js
@@ -3,6 +3,8 @@ process.env.LOG_SQL_QUERIES = '1'
 process.env.DATABASE_URL = process.env.DATABASE_URL || 'postgres://localhost:5432/lg-curriculum-test'
 process.env.HUBSPOT_API_KEY = process.env.HUBSPOT_API_KEY || 'FAKE_HUBSPOST_API_KEY'
 process.env.JWT_PUBLIC_KEY = 'FAKE_PUBLIC_KEY'
+process.env.IDM_BASE_URL = 'http://idm.learnersguild.test'
+process.env.ECHO_BASE_URL = 'http://echo.learnersguild.test'
 
 const chaiHttp = require('chai-http')
 const webServer = require('./web-server')
@@ -11,10 +13,6 @@ global.chai = require('chai')
 global.expect = chai.expect
 
 chai.use(chaiHttp)
-
-global.webServer = () =>
-  chai.request(webServer)
-
 
 
 chai.Assertion.addMethod('haveDuplicates', function() {
@@ -36,3 +34,29 @@ chai.Assertion.addMethod('haveDuplicates', function() {
     dups // actual
   );
 })
+
+global.createWebserverAgent = function(){
+  const agent = chai.request.agent(webServer)
+
+  agent.loginAs = function(user, callback){
+    return this
+    .post('/__login')
+    .send(user)
+    .end(function(error, response){
+      if (error) return callback(error)
+      // expect(this).to.have.cookie('__user');
+      callback(null, callback)
+    })
+  }
+
+  agent.loginAsLearner = function(callback){
+    return this.loginAs({
+      id: 'FAKE_USER_ID',
+      name: 'Fake User',
+      handle: 'fakeuser',
+      roles: ['learner'],
+    }, callback)
+  }
+
+  return agent
+}
--- web-server/routes/goals.test.js
+++ web-server/routes/goals.test.js
@@ -2,55 +2,61 @@
 const goalsById = require('../../goals/goals')
 const goals = Object.values(goalsById)
 
-describe('/api/goals/index.json', function(){
-  it('should render all goals', function(done){
-    webServer()
-      .get('/api/goals/index.json')
-      .end(function(error, response) {
-        if (error) { return done(error) }
-        expect(response).to.have.status(200)
-        expect(response.body).to.deep.equal({ goals })
-        done()
-      });
+describe('/api/goals', function(){
+  beforeEach(function(){
+    this.webServer = createWebserverAgent()
   })
-})
 
-describe('/api/goals/:goalId.json', function(){
-  context('when the goal id is valid', function(){
-    let goalId
-    beforeEach(function(){
-      goalId = Object.keys(goalsById)[0]
-      expect(goalId).to.match(/^\d+$/)
-    })
-
-    it('should render the goal', function(done){
-      webServer()
-        .get(`/api/goals/${goalId}.json`)
+  describe('/index.json', function(){
+    it('should render all goals', function(done){
+      this.webServer
+        .get('/api/goals/index.json')
         .end(function(error, response) {
           if (error) { return done(error) }
           expect(response).to.have.status(200)
-          expect(response.body).to.deep.equal(goalsById[goalId])
+          expect(response.body).to.deep.equal({ goals })
           done()
         });
     })
   })
 
-  context('when the goal id is invalid', function(){
-    let goalId
-    beforeEach(function(){
-      goalId = '3746374'
+  describe('/:goalId.json', function(){
+    context('when the goal id is valid', function(){
+      let goalId
+      beforeEach(function(){
+        goalId = Object.keys(goalsById)[0]
+        expect(goalId).to.match(/^\d+$/)
+      })
+
+      it('should render the goal', function(done){
+        this.webServer
+          .get(`/api/goals/${goalId}.json`)
+          .end(function(error, response) {
+            if (error) { return done(error) }
+            expect(response).to.have.status(200)
+            expect(response.body).to.deep.equal(goalsById[goalId])
+            done()
+          });
+      })
     })
 
-    it('should render a 404', function(done){
-      webServer()
-        .get(`/api/goals/${goalId}.json`)
-        .end(function(error, response) {
-          expect(response).to.have.status(404)
-          expect(response.body).to.deep.equal({
-            error: `Could not find goal with id: ${goalId}`
-          })
-          done()
-        });
+    context('when the goal id is invalid', function(){
+      let goalId
+      beforeEach(function(){
+        goalId = '3746374'
+      })
+
+      it('should render a 404', function(done){
+        this.webServer
+          .get(`/api/goals/${goalId}.json`)
+          .end(function(error, response) {
+            expect(response).to.have.status(404)
+            expect(response.body).to.deep.equal({
+              error: `Could not find goal with id: ${goalId}`
+            })
+            done()
+          });
+      })
     })
   })
 })
